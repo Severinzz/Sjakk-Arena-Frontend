046e462fad6a3d835451a5f7456012941d60c8ce
Severinzz <36744838+Severinzz@users.noreply.github.com>
2020-02-06 10:23:33 +0100

    Cleanup
    Fixed an overlooked code style error

--- src/components/MenuTile.vue
+++ src/components/MenuTile.vue
@@ -1,7 +1,8 @@
 <template>
     <v-card :color="color"
             :to="to"
-            :tile=true width="20vw" height="20vw" max-height="300px" max-width="300px"
+            :tile=true
+            width="20vw" height="20vw" max-height="300px" max-width="300px"
             min-height="230px" min-width="230px" class="ma-2" >
       <v-card-title class="justify-center">
         <span>{{ tileText }}</span>
