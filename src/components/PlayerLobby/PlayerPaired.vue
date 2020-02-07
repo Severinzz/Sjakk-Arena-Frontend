039e580901935be2318606d0e0133eaf2a662962
JuleNissen <jahn_willy97@hotmail.com>
2020-02-07 11:56:23 +0100

    Loading circle when pariring -indeterminate spinning
    Page for paired and something happens while not paired, + error message if pairing variable dissapears for whatever.

---
+++ src/components/PlayerLobby/PlayerPaired.vue
@@ -0,0 +1,26 @@
+<template>
+    <span>
+      <p class="gameDetail">Neste parti: {{TnextOpponent}}</p>
+      <p class="gameDetail">Farge: {{Tcolour}}</p>
+      <p class="gameDetail">Bord: {{Tbord}}</p>
+    </span>
+</template>
+
+<script>
+export default {
+  name: 'PlayerPaired',
+  data () {
+    return {
+      TnextOpponent: 'Magnus Carlsen',
+      Tcolour: 'Sort',
+      Tbord: '1'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .gameDetail{
+    margin-top: 1.2em;
+  }
+</style>
--- src/components/PlayerLobby/PlayerPlaying.vue
+++ src/components/PlayerLobby/PlayerPlaying.vue
@@ -12,9 +12,21 @@
           <h1>{{ Tname }}</h1>
           <h2 class="gameDetail">{{Usernamne}}</h2>
           <h2 class="gameDetail">Poeng: {{Tpoints}}</h2>
-          <p class="gameDetail">Neste parti: {{TnextOpponent}}</p>
-          <p class="gameDetail">Farge: {{Tcolour}}</p>
-          <p class="gameDetail">Bord: {{Tbord}}</p>
+          <div v-if="paired">
+            <PlayerPaired></PlayerPaired>
+          </div>
+          <div v-else-if="!paired">
+            <p class="gameDetail">Please wait while we try to pair you up!</p>
+            <v-progress-circular
+              :size="70"
+              :width="7"
+              color="indigo"
+              indeterminate
+            ></v-progress-circular>
+          </div>
+          <div v-else>
+            <p>System is unable to pair you up, please try again.</p>
+          </div>
           <p class="gameDetail body-2">Spille tid: {{Tstart}} -> {{Tend}} </p>
           <v-spacer />
         </v-card-text>
@@ -24,19 +36,22 @@
 </template>
 
 <script>
+import PlayerPaired from './PlayerPaired'
+
 export default {
   name: 'PlayerPlaying',
+  components: {
+    PlayerPaired
+  },
   data() {
     return {
       Tname: 'NTNU Open',
       Tstart: '13:00',
       Tend: '15:30',
       Tpin: '13376',
-      Tpoints: '13',
-      TnextOpponent: 'Magnus Carlsen',
       Usernamne: 'Ola Nordmann',
-      Tcolour: 'Sort',
-      Tbord: '1'
+      Tpoints: '13',
+      paired: false
     }
   }
 }
