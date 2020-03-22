0266ff7591cb086d3242824ed0566fdb8b534f92
Severinzz <36744838+Severinzz@users.noreply.github.com>
2020-03-22 13:44:25 +0100

    Add comments and logic
    Changed name of some variables to for a better fit
    Added logic for jumping page
    Emitts event button/page change
    Added comments

--- src/components/PaginationButtons.vue
+++ src/components/PaginationButtons.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
-    <v-btn class="left"> &laquo; </v-btn>
+    <v-btn
+      class="left"
+      @click="jumpLeft"
+      v-if="activeButton !== 1">
+      &laquo;
+    </v-btn>
     <v-btn
       v-for="page in visibleButtons"
       :key="page"
     @click="changePage(page)"
-    :class="page === activePage ?'active-button' : ''"
+    :class="page === activeButton ?'active-button' : ''"
     >{{ page }}</v-btn>
-    <v-btn class="right"> &raquo; </v-btn>
-      <p>Side {{ activePage }} av {{ lastButton }}</p>
+    <v-btn
+      class="right"
+      @click="jumpRight"
+    v-if="activeButton !== lastButton">
+      &raquo;
+    </v-btn>
+      <p>Side {{ activeButton }} av {{ lastButton }}</p>
   </div>
 </template>
 
@@ -22,41 +32,71 @@ export default {
   },
   data() {
     return {
-      activePage: 1,
+      activeButton: 1,
       lastButton: this.numberOfItems / this.prPage
     }
   },
   methods: {
     changePage(page) {
-      this.activePage = page
+      this.activeButton = page
+      this.$emit('pageChanged', page)
     },
+    // Returns if there is any buttons to the left outside of the visible ones
     hasButtonsLeft() {
       return this.minButtonNr() >= 1
     },
+    // Checks if there is any buttons to the right outside of the visible ones
     hasButtonsRight() {
       return this.maxButtonNr() <= this.lastButton
     },
+    // Returns the index of the lowest visible number
     minButtonNr() {
-      return Math.floor(this.activePage - (this.maxVisibleButtons / 2))
+      return Math.floor(this.activeButton - (this.maxVisibleButtons / 2))
     },
+    // Returns the highest number that should be visible.
     maxButtonNr() {
-      return Math.floor(this.activePage + (this.maxVisibleButtons / 2))
+      return Math.floor(this.activeButton + (this.maxVisibleButtons / 2))
+    },
+    /*
+     Jumps to the first button outside of the visible left range, if button 1 is visible that wil be jumped to.
+     */
+    jumpLeft() {
+      if (this.minButtonNr() <= 0) {
+        this.changePage(1)
+      } else {
+        this.changePage(this.minButtonNr())
+      }
+    },
+    /*
+    Jumps to the first button outside of the visible right range,
+    if button last button is visible that wil be jumped to.
+     */
+    jumpRight() {
+      if (this.maxButtonNr() >= this.lastButton) {
+        this.changePage(this.lastButton)
+      } else {
+        this.changePage(this.maxButtonNr() + 1)
+      }
     }
   },
   computed: {
     visibleButtons() {
       let button = 1
       let buttonsArr = []
+      // Creates an array of all the buttons.
       while (buttonsArr.length < this.lastButton) {
         buttonsArr.push(button)
         button++
       }
       switch (true) {
+        // If there are no buttons to the left of the visible range
         case !this.hasButtonsLeft():
           return buttonsArr.slice(0, this.maxVisibleButtons)
+        // No buttons to the right of the visible range
         case !this.hasButtonsRight():
           return buttonsArr.slice(this.lastButton - this.maxVisibleButtons, this.lastButton)
         default:
+          // There are buttons to both right and left of the visible range.
           return buttonsArr.slice(this.minButtonNr(), this.maxButtonNr())
       }
     }
