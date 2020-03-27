396c4378863c659e2420aec300d6b126f93518b9
JuleNissen <jahn_willy97@hotmail.com>
2020-03-27 12:16:17 +0100

    update invalidGames component

--- src/components/InvalidGames.vue
+++ src/components/InvalidGames.vue
@@ -1,17 +1,21 @@
 <template>
-<v-card height="auto" class="pa-0">
+<v-card height="auto">
   <v-layout column fill-height>
     <v-toolbar dense class="primary">
       <v-toolbar-title>Spill med ugyldige resultater</v-toolbar-title>
     </v-toolbar>
     <v-container fluid fill-height>
       <v-layout>
-        <ul>
-          <li v-for="(games, index) in computedObj" :key="index"></li>
-          <h6 class="body-1">Parti: {{game_id}}, Bord: {{table}}</h6>
-          <p class="body-2">Spillere: {{white_player_name}} og {{black_player_name}}</p>
-          <p class="body-2">Hvit spiller poeng: {{white_player_points}}</p>
-        </ul>
+        <table justify-center>
+          <tr></tr>
+          <th>Parti ID, Bord, Spillere, Poeng til hvit spiller</th>
+          <tr></tr>
+          <tr v-for="(Game, index) in gameList" v-bind:key="index">
+            <th class="body-1"><strong>Parti ID: </strong>{{Game.game_id}}, <strong>Bord: </strong>{{Game.table}}</th>
+            <td class="body-1"><strong>Spillere:</strong> {{Game.white_player_name}} og {{Game.black_player_name}}</td>
+            <td class="body-1">, <strong>Hvit</strong> spiller poeng: {{Game.white_player_points}}</td>
+          </tr>
+        </table>
       </v-layout>
     </v-container>
   </v-layout>
@@ -19,16 +23,19 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
   name: 'InvalidGames',
   data () {
     return {
       limit: 10,
+      timeInderval: 5000,
       disapprovedGames: [
         {
           game_id: 1,
           white_player_name: 'Magnus',
-          black_player_name: 'Dumbledore',
+          black_player_name: 'Albus Dumbledore',
           table: 4,
           white_player_points: 1
         },
@@ -63,8 +70,32 @@ export default {
       ]
     }
   },
-  computed () {
-    return this.limit ? this.disapprovedGames.slice(0, this.limit) : this.disapprovedGames
+  methods: {
+    ...mapActions([
+      'fetchInvalidGames'
+    ]),
+    loadInvalidGames () { // TODO: gjør om fram pulling til websocket updates
+      const VM = this
+      this.intervalID = setInterval(async function () {
+        await VM.fetchInvalidGames().then(res => {
+          VM.disapprovedGames = res.data
+          console.log(VM.disapprovedGames)
+        }).catch( err => {
+          throw err.response
+        })
+      }, this.timeInterval)
+    }
+  },
+  computed: {
+    gameList () {
+      return this.limit ? this.disapprovedGames.slice(0, this.limit) : this.disapprovedGames
+    }
+  },
+  created() {
+    this.loadInvalidGames()
+  },
+  destroyed() {
+    clearInterval(this.intercal)
   }
 }
 </script>
