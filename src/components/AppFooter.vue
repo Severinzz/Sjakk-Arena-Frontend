2264763b6d3e00ecd279a134952f12fe9443a9a7
JuleNissen <jahn_willy97@hotmail.com>
2020-04-21 12:27:56 +0200

    about page linked in footer, opens in new tab

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,11 @@
 <template>
   <v-footer color="#f5f5f5">
+    <p>Trenger du hjelp eller ønsker å vite mer om applikasjonen trykk
+      <strong>
+        <router-link to="/about" target="_blank">her.
+        </router-link>
+      </strong>
+    </p>
     <v-spacer></v-spacer>
     <div>&copy; {{ new Date().getFullYear() }}</div>
   </v-footer>
